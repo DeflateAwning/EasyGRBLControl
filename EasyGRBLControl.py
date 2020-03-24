ccec6c4e83ee5ad957f2b18e41669e1e33b1b11c
Parker Link <parker@example.com>
2020-03-24 14:47:17 -0600

    Add macro/probing system, add better receiving of info from buffer

--- EasyGRBLControl.py
+++ EasyGRBLControl.py
@@ -28,11 +28,34 @@ if len(sys.argv) == 2:
 	streamFile(filename)
 
 probeMacro = """
-G0 Z0
+G90
+G38.2 Z-{0[maxdepth]} F{0[speed]}
+G92 Z{0[thickness]}
+G91
+G0 Z8
 """
+# Probes, stop on contact. Sets the current height to the thickness. Sets to relative movement. Raises 8mm to get plate out.
 
 promptTemplate = "({mode}) >>> "
 modeOptions = {"abs/rel": "abs"}
+
+
+def getIncomingSerial():
+	"""
+	Gets all currently waiting incoming serial data from the serial connection.
+
+	If it's just an 'ok', it puts it on the same line. Otherwise, it puts it on a new line.
+	"""
+	out = ""
+	while serialConnection.in_waiting == 0:
+		#print("Delaying while the machine sends an output")
+		time.sleep(0.05)
+
+	while serialConnection.in_waiting > 0:
+		out += serialConnection.read_until().strip() + "\n"
+	out = out.strip()
+	return out
+
 
 def streamFile(filename=None):
 	""" This command can be called from the main prompt, prompts for file location and streams file """
@@ -45,6 +68,7 @@ def streamFile(filename=None):
 		filename = tkFileDialog.askgcodeFilename(initialdir="/home/parker/Dropbox/1Parker/")
 
 	if raw_input('Press enter to begin streaming "' + str(filename) + '" or type quit and press enter to go back to prompt.'):
+		print("Cancelled streaming.")
 		return 1
 
 	# Count the number of lines in the file (for estimation purposes)
@@ -64,32 +88,52 @@ def streamFile(filename=None):
 		l = line.strip() # Strip all EOL characters for streaming
 		curLineNumber += 1
 
-		print 'Sending: ' + l,
+		print('Sending: ' + l)
 		serialConnection.write(l + '\n') # Send g-code block to grbl
-		incomingSerialLine = serialConnection.readline() # Wait for grbl response with carriage return
+		incomingSerial = getIncomingSerial()
 		
-		if "Grbl" in incomingSerialLine and "for help" in incomingSerialLine:
+		if "Grbl" in incomingSerial and "for help" in incomingSerial:
 			# There was a reset (ie Emergency Stop), exit steaming
-			print ("EMERGENCY STOPPING STREAMING, likely an emergency stop or reset")
+			print("EMERGENCY STOPPING STREAMING, likely an emergency stop or reset")
 			break
 
-		print ' : ' + incomingSerialLine.strip() + " : \t\t" + str(curLineNumber) + "/" + str(totalLines) + "=" + str(round(curLineNumber*100/totalLines, 2)) + "% in " + str(round((time.time()-startTime)/60, 2)) + " min"
+		print('Received: ' + incomingSerial + " : \t\t" + str(curLineNumber) + "/" + str(totalLines) + "=" + str(round(curLineNumber*100/totalLines, 2)) + "% in " + str(round((time.time()-startTime)/60, 2)) + " min")
 
 	# Close g-code 
 	gcodeFile.close()
 
 	print("Done streaming file in {:.2f} minutes.".format((time.time() - startTime)/60))
 
-def sendMacro():
-	print("sendMacro not currently implemented")
+def sendMacro(commandLines):
+	"""
+	Sends a command with multiple lines
+	"""
+
+	for line in commandLines.split('\n'):
+		if len(line) == 0:
+			continue
+
+		line = line.strip()
+		print("Send: " + line)
+		sendCommand(line)
 
 def sendCommand(l):
 	"""
-	Sends a single line of GCode to the machine, generally from prompt
+	Sends a single line of GCode to the machine, generally from prompt.
+
+	Also, sets the current movement mode (G90/G91 for abs vs. rel movement)
 	"""
+	l = l.strip()
+
+	if l.lower() == "g90":
+		modeOptions["abs/rel"] = "abs"
+	elif l.lower() == "g91":
+		modeOptions["abs/rel"] = "rel"
+
 	serialConnection.write(l + '\n') # Send g-code block to grbl
-	incomingSerialLine = serialConnection.readline() # Wait for grbl response with carriage return
-	print ' : ' + incomingSerialLine.strip()
+	#incomingSerial = serialConnection.readline() # Wait for grbl response with carriage return
+	incomingSerial = getIncomingSerial()
+	print("\tReceived: " + incomingSerial)
 
 def closeConnections():
 	# Close serial port
@@ -101,7 +145,7 @@ def printHelp():
 	print("""
 Use the following commands:
 	- help: displays this help message
-	- probe thickness speed=25 maxdepth=10: probes worksurface at current XY position, to a maximum depth of "maxdepth" (default 10) at a speed of "speed" (default 25), moves tool back up, and sets current height to the touchplate's thickness, "thickness"
+	- probe <thickness of touch plate=19.25> <speed=25> <maxdepth=10>: probes worksurface at current XY position (default shown), moves tool back up, and sets the appropriate height (touchplate thickness)
 	- send: prompt for filename and stream that file (prompts for confirmation)
 	- abs/rel: absolute coordinate mode/relative coordinate mode (G90=abs, G91=rel)
 	- quit: closes serial connection and quits
@@ -110,10 +154,12 @@ Any other command starting with '$' or 'G' will be sent directly to GRBL. Any ot
 
 Example Commands:
 	- help
+	- probe
 	- probe 12
 	- probe 20 30 5
 	- send
 	- abs/rel
+	- G0 X10
 	- quit
 """)
 
@@ -144,22 +190,26 @@ while True:
 
 	elif command.startswith("probe"):
 		commandSplit = command.split(" ")
+		probeOptions = {"thickness": 19.25, "speed": 25, "maxdepth": 10}
 		try:
-			thickness = commandSplit[1]
+			probeOptions["thickness"] = float(commandSplit[1])
 		except:
-			print("No/invalid thickness provided.")
-			continue
+			pass
 		try:
-			speed = commandSplit[2]
+			probeOptions["thickness"] = float(commandSplit[2])
 		except:
-			speed = 25
+			pass
 		try:
-			maxdepth = commandSplit[3]
+			probeOptions["thickness"] = float(commandSplit[3])
 		except:
-			maxdepth = 10
-		sendMacro(probeMacro.replace("thickness", thickness).replace("speed", speed).replace("maxdepth", maxdepth))
+			pass
+		if not raw_input("Press enter to confirm sending probe with options: {} >>> ".format(probeOptions)):
+			sendMacro(probeMacro.format(probeOptions))
+			print("Probing Complete.")
+		else:
+			print("Cancelled Probe.")
 
-	elif command[0] == "$" or command[0] == "g" or command[0] == "m":
+	elif command.startswith("$") or command.startswith("g") or command.startswith("m"):
 		# Accept Single G-Codes and M-Codes
 		sendCommand(command)
 
--- README.md
+++ README.md
@@ -3,19 +3,28 @@ Control GRBL from the command line with a minimalistic interface, while allowing
 This program is used to stream GCode files to a GRBL-running CNC machine. It also has a few predefined macros to make life easier.
 
 ## Current Help Message
+The follow help message describes most of the features/commands present in this program.
 ```
 Use the following commands:
 	- help: displays this help message
-	- probe thickness speed=25 maxdepth=10: probes worksurface at current XY position, to a maximum depth of "maxdepth" (default 10) at a speed of "speed" (default 25), moves tool back up, and sets current height to the touchplate's thickness, "thickness"
+	- probe <thickness of touch plate=19.25> <speed=25> <maxdepth=10>: probes worksurface at current XY position (default shown), moves tool back up, and sets the appropriate height (touchplate thickness)
 	- send: prompt for filename and stream that file (prompts for confirmation)
+	- abs/rel: absolute coordinate mode/relative coordinate mode (G90=abs, G91=rel)
 	- quit: closes serial connection and quits
 
 Any other command starting with '$' or 'G' will be sent directly to GRBL. Any other command will simply give an error.
 
 Example Commands:
 	- help
+	- probe
 	- probe 12
 	- probe 20 30 5
 	- send
+	- abs/rel
+	- G0 X10
 	- quit
 ```
+
+## To Do
+* Change to Python3
+* Improve readline to readlines system in reading buffer
